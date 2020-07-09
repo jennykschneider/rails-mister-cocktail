f4689c36f63446485f242f0f1242ae86b4b1bb98
Jennifer Schneider <je.schneider@zeppelin-university.net>
2020-07-09 16:43:05 +0200

    send help

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic water")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "apple")
+Ingredient.create(name: "sugar")
